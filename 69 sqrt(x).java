78b2d3e144beadb04fc0f92482adb05d91c09959
Vaishnavi <140370680+vaishnavighodake1@users.noreply.github.com>
2026-08-12 09:22:37 +0530

    69 sqrt(x).java

---
+++ 69 sqrt(x).java
@@ -0,0 +1,15 @@
+class Solution {
+    public int mySqrt(int x) {
+        long l = 0, r = x;
+
+        while (l <= r) {
+            long mid = (l + r) / 2;
+
+            if (mid * mid == x) return (int)mid;
+            if (mid * mid < x) l = mid + 1;
+            else r = mid - 1;
+        }
+
+        return (int)r;
+    }
+}
